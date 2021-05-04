3b45104bca96e60c89aecef4db47834cc404db45
Cesar73 <cesar_7391@outlook.com>
2021-05-04 00:20:02 -0500

    Primer commit

---
+++ Tarea 01.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text; 
+using System.Threading.Tasks;
+//namespaces
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Flores López César Elí");
+            Console.WriteLine("Linea 2 de descripción");
+            Console.WriteLine("Linea 3 de descripción");
+
+            //MANTIENE ABIERTA LA CONSOLA HASTA QUE SE PRESIONE ALGUNA TECLA
+            Console.ReadKey();
+        }
+    }
+}
