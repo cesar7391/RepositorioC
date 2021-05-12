ade62e2403cd60d56cead3001cef64423debd5f4
Cesar73 <cesar_7391@outlook.com>
2021-05-12 10:59:37 -0500

    añadida linea problema 2

--- Examen/Problema_2.cs
+++ Examen/Problema_2.cs
@@ -15,14 +15,17 @@ namespace Examen
             TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
             String[] arreglo = { "hola mundo", "pepe pecas pica papas", "la luna es bonita" };
             List<String> lista = new List<string>();
-            
-            for(int i = 0; i < arreglo.Length; i++)
+
+            for (int i = 0; i < arreglo.Length; i++)
             {
+                Console.WriteLine(arreglo[i]);
                 transformar = ti.ToTitleCase(arreglo[i]);
                 lista.Add(transformar);
             }
 
-            for(int i = 0; i < lista.Count; i++)
+            Console.WriteLine();
+
+            for (int i = 0; i < lista.Count; i++)
             {
                 Console.WriteLine(lista[i]);
             }
